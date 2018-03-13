d88c7fddbed547eee0813fcc8cfb5b3588f05776
Qiyang-liu <2017060204018@std.uestc.edu.cn>
2018-03-13 10:09:01 +0800

    isPrime

---
+++ level1/p02_isPrime/isPrime.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int judge(int num)
+{
+	int i,judge=0;
+	for (i=2;i<(num/2+1);i++)
+	{
+		judge=num%i;
+		if (judge==0)
+		{
+			break;
+		}
+		else continue;
+	}
+	if (num==2||num==3)
+	{
+		judge=1;
+	}
+	return judge;
+}
+
+int main()
+{
+	int num;
+	printf("Please enter a nunber:\n");
+	scanf("%d",&num);
+	if (judge(num)==0) 
+	{
+		printf("%d is not a prime number.\n",num);
+	}
+	else
+	{
+		printf("%d is a prime number.\n",num);
+	}
+	return 0;
+}
