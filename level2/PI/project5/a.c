937a2973b6f7a80c03665afe8dfdbf9bb6fe5733
梁子辉 <876459060@qq.com>
2018-03-14 17:24:09 +0800

    level2/PI基本完成cpu部分的人脑优化，跑完1M数据，好不容易达到柱爷水平（可能稍好一点点，hhh）

--- level2/PI/project5/a.c
+++ level2/PI/project5/a.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<time.h>
-#define N 1000000
-#define omp_threads 16
+#define N 100000
+#define omp_threads 32
 #define MAXN N*4
 #define max(x,y) (x<y?y:x)
 #define MOD2 1073741824
@@ -12,12 +11,13 @@
 #define MOD10 100000000
 #include<omp.h>
 #include<mpi.h>
-int tot,r,totans,lena,lenb,lend,lenc;
+int tot,r,totans,lena,len,lenb,lend,lenc;
 long long a[N/3+1],b[N/3+1],c[N/3+1],d[N/3+1];
 long long ans[N/3+1],pi[N/3+1];
 void pre(long long*a,int x,int*len){
 	x=MAXN-4-x;
 	a[*len=x/length2]=(1<<(x%length2));
+	x/=length2;
 }
 void divide(long long*a,int x,int*len){
 	for (int i=*len;i>=1;i--){
@@ -28,12 +28,12 @@ void divide(long long*a,int x,int*len){
 	while (!a[*len]&&*len)*len--;
 }
 void merge1(long long*a,long long*ans,int len, int* len1){
-	for (int i=0;i<len;i++){
+	for (int i=0;i<=len;i++){
 		ans[i]+=a[i],a[i]=0;
 		ans[i+1]+=(ans[i]>>length2);
 		ans[i]&=(MOD2_1);
 	}
-	ans[len]+=a[len],a[len]=0;
+	len++;
 	while (ans[len]>MOD2_1){
 		ans[len+1]+=(ans[len]>>length2);
 		ans[len]&=(MOD2_1);
@@ -42,45 +42,68 @@ void merge1(long long*a,long long*ans,int len, int* len1){
 	*len1=(*len1>len?*len1:len);
 }
 void merge(){
-	for (int i=0;i<totans;i++){
+	for (int i=0;i<=len;i++){
 		ans[i]+=a[i]-b[i]-c[i]-d[i];
-		if (ans[i]<0){
-			ans[i+1]--,ans[i]+=MOD2;
-			while (ans[i]<0)ans[i+1]--,ans[i]+=MOD2;
-		}else{
-			ans[i+1]+=(ans[i]>>length2);
-			ans[i]&=(MOD2_1);
-		}
+		while (ans[i]<0)ans[i+1]--,ans[i]+=MOD2;
+		ans[i+1]+=(ans[i]>>length2);
+		ans[i]&=(MOD2_1);
 	}
-	ans[totans]+=a[totans]-b[totans]-c[totans]-d[totans];	
-	while (!ans[totans])totans--;
+	len++;
+	while (ans[len]>MOD2_1){
+		ans[len+1]+=(ans[len]>>length2);
+		ans[len]&=(MOD2_1);
+		len++;
+	}
+	totans=(totans>len?totans:len);
 }
-void dividepi(int x){
-	int y=(1<<x)-1;
-	for (int i=tot;i>=1;i--){
-		pi[i-1]+=(pi[i]&y)*MOD10;
-		pi[i]>>=x;
-	}
-	pi[0]>>=x;
-}
-void turn(){
-	tot=N/8+1;
-	for (int i=0;i<=totans;i++){
-		dividepi(length2);
-		pi[tot]+=ans[i];
-	}
-	int x=0,y=ans[totans];
-	while (y)x++,y>>=1;
-	dividepi(x-2);
+void twice(){
+	for (int i=0;i<=tot;i++)
+		pi[i]<<=length2;
 	for (int i=0;i<=tot;i++)
 		pi[i+1]+=pi[i]/MOD10,pi[i]%=MOD10;
-	while (!pi[tot])tot--;
+	while (pi[tot+1]){
+		++tot;
+		pi[tot+1]+=pi[tot]/MOD10;
+		pi[tot]%=MOD10;	
+	}
 }
-int main(int argc,char*argv[]){
+void turn(){
+	for (int i=totans;i>=0;i--){
+		twice();
+		if (!r&&ans[i]){
+			r=i*length2;
+			int x=ans[i];
+			while (x)r++,x/=2;
+		}
+		pi[0]+=ans[i];
+	}
+	for (int i=0;i<=tot;i++)
+		pi[i+1]+=pi[i]/MOD10,pi[i]%=MOD10;
+	while (pi[tot+1]){
+		++tot;
+		pi[tot+1]+=pi[tot]/MOD10;
+		pi[tot]%=MOD10;	
+	}
+	int x=tot;
+	tot=N/3+1;
+	for (int i=x;i>=0;i--)
+		pi[--tot]=pi[i];
+	for (int i=tot-1;i>=0;i--)pi[i]=0;
+	tot=N/3;
+}
+void dividepi(int x){
+	for (int i=tot;i>=0;i--){
+		if (i)pi[i-1]+=pi[i]%x*MOD10;
+		pi[i]/=x;
+	}
+	for (int i=0;i<=tot;i++)
+		pi[i+1]+=pi[i]/MOD10,pi[i]%=MOD10;
+	while (!pi[tot]&&tot)tot--;
+}
+int main(int argc,char*argv[]){	
 	int my_rank;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	double start = MPI_Wtime();
 	if (my_rank==0)
 	{
 #pragma omp parallel for num_threads(omp_threads) 
@@ -206,15 +229,20 @@ int main(int argc,char*argv[]){
 		MPI_Recv(b,N/3+1,MPI_LONG_LONG,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 		MPI_Recv(c,N/3+1,MPI_LONG_LONG,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
 		MPI_Recv(d,N/3+1,MPI_LONG_LONG,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-		totans=lena;
+		len=lena;
 		merge();
 		turn();
+		for (int i=0;i<=r-2;)
+			if (i+length2<=r-2){
+				dividepi(MOD2);
+				i+=length2;
+			}else
+				dividepi(1<<(r-2-i)),i=r-2+1;
 		int sum=0;
 		long long x=1;
 		while (x<pi[tot])x*=10;
 		x/=100;
 		freopen("pi.out","w",stdout);
-		printf("Time: %fs\n",MPI_Wtime()-start);
 		printf("3.");
 		while (x){
 			printf("%d",pi[tot]/x%10);
