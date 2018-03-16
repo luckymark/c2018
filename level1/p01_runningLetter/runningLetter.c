ac0192174bb4519c776f0ac570acc70c9591925e
Chen <34164851+MrNobodyBee@users.noreply.github.com>
2018-03-16 20:00:35 +0800

    修改runningLetter

--- level1/p01_runningLetter/runningLetter.c
+++ level1/p01_runningLetter/runningLetter.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
-#define screen_l 50
+#define SCREEN_WID 50
+#define SLEEP_TIME 100
 
 int main()
 {
 	system("mode con cols=50 lines=20");
-	int l_w;
-	char words[36];
+	int length_word;
+	char words[50];
 	scanf("%s", &words);
-	l_w = strlen(words);
-	for (int space_l = 0; space_l <= screen_l - l_w; space_l++)
+	length_word = strlen(words);
+	for (int space_left = 0; space_left <= SCREEN_WID - length_word; space_left++)
 	{
-		for (int n = 0; n < space_l; n++)
+		for (int n = 0; n < space_left; n++)
 		{
 			printf(" ");
 		}
@@ -21,14 +22,14 @@ int main()
 		Sleep(100);
 		system("CLS");
 	}
-	for (int space_l = screen_l - l_w; space_l > 0; space_l--)
+	for (int space_left = SCREEN_WID - length_word; space_left > 0; space_left--)
 	{
-		for (int n = space_l; n > 0; n--)
+		for (int n = space_left; n > 0; n--)
 		{
 			printf(" ");
 		}
 		printf("%s", words);
-		Sleep(100);
+		Sleep(SLEEP_TIME);
 		system("CLS");
 	}
 	return 0;
