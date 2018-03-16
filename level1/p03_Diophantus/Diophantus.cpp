3a2f97ef124c419fd58d68ae22caab1e4c66cd9a
Gao En <37091179+SillyG@users.noreply.github.com>
2018-03-17 01:43:17 +0800

    Add files via upload

---
+++ level1/p03_Diophantus/Diophantus.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	//suppose his son is x years old when he died
+	//we can easly get the eqution : 2*x*11/28 + 5 + 2*x = x - 4
+	float x;
+	x=9*17/(6.0);
+	printf("%f",2*x);
+} 
