5838b23131e51d3695b143ba9ed17e090286307f
Gao En <37091179+SillyG@users.noreply.github.com>
2018-03-18 22:24:21 +0800

    Update hanoi.cpp

--- level1/p08_hanoi/hanoi.cpp
+++ level1/p08_hanoi/hanoi.cpp
@@ -5,13 +5,13 @@
 
 void move(int n,char a,char b,char c){
 	if(n==1){
-		printf("%c --> %c\n",a,c);
+		printf("%c --> %c\n",a,c);	
 		return;
 	}
 	else{
-		move(n-1,a,c,b);
-		printf("%c --> %c\n",a,c);
-		move(n-1,b,a,c);
+		move(n-1,a,c,b);		//run the n-1 situation, but target is B
+		printf("%c --> %c\n",a,c);	//move the n th plate
+		move(n-1,b,a,c);		//move to C from B (n-1 situation)
 	}
 } 
 
