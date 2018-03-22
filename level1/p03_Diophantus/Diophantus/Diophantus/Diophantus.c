52eb9e394fe41a98272228e8f95b48e9bdcc0658
大笨猪 <2490683205@qq.com>
2018-03-22 13:08:45 +0800

    水仙花数

--- level1/p03_Diophantus/Diophantus/Diophantus/Diophantus.c
+++ level1/p03_Diophantus/Diophantus/Diophantus/Diophantus.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
+int main(void)
 {
 	for(int Text = 1; ;Text++)
 	if ((Text * 12) % 7 == 0 && (Text * 2 + Text + (12 * Text) / 7 + 5 + 6 * Text + 4) == 12 * Text)
