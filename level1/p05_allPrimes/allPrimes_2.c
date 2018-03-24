9f9f632ed5a1b7c5edb7a42497289260dfaa2d2b
smallhhy <37150104+smallhhy@users.noreply.github.com>
2018-03-24 22:16:48 +0800

    Create allPrimes_2.c

---
+++ level1/p05_allPrimes/allPrimes_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#define N 1000
+int main(void)
+{  
+    int time0=clock();
+	  int i,mod,num;
+    for(num=2;num<=N;num++)
+    {
+        i=2;
+        do
+        {
+            mod=(num%i);
+            i++;
+		    }while(mod!=0);
+        if(i>=num)
+        printf("%d ",num);
+        else;
+    }
+	  int time1=clock();
+	  printf("\nFinished! Total time is %dms.\n",time1-time0);
+	  getchar();
+    return 0;
+}
