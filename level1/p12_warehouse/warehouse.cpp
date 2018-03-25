213d9d67563f80b1ce1b5af8fe4616b3f27fd3b4
Wolf-broken <37091278+Wolf-broken@users.noreply.github.com>
2018-03-25 21:25:40 +0800

    修改提交warehouse

---
+++ level1/p12_warehouse/warehouse.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	printf("菜单： \n");
+	printf("\t1.显示存货列表\n");
+	printf("\t2.入库\n");
+	printf("\t3.出库\n");
+	printf("\t4.退出程序\n");
+	while(1){
+		char a=getchar();
+	switch(a){
+		case '1':printf("123123");
+		case '2':printf("123123");
+		case '3':printf("123123");
+		case '4': break;
+	}  
+}
+	return 0;
+}
