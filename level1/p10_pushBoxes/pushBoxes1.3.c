3abf4344f89b54ccf632abf107e4016b41668771
CDsxy <1773844883@qq.com>
2018-03-25 09:05:31 +0800

    优化版推箱子

--- level1/p10_pushBoxes/pushBoxes1.3.c
+++ level1/p10_pushBoxes/pushBoxes1.3.c
@@ -1,34 +1,34 @@
 #include<stdio.h> 
-#include<conio.h>
+#include<conio.h>          //getch的原型 
 #include<stdlib.h>
 #include<windows.h>
 #define WIDE 9
 #define LEN  10
-#define Boxpush MAP
 #define ENDJUDGE(c) MAP[end[c].x][end[c].y] != 'B'  //终点判断 
 
-struct COD{
+struct COD{                            //指明坐标的结构 
 	int x;
 	int y;
 };
-struct COD end[4];                   //终点数组 
-int cnt=1;
+struct COD end[4];                   //终点数组记录每一关卡的终点 
+int cnt=1;                           //用于异常检测 
 
-char MAP[WIDE][LEN]; 
+char MAP[WIDE][LEN];
+void Initmap(void);                 //将数组全部初始化为空格 
 struct COD Initmap_1(void);
 struct COD Initmap_2(void);
 struct COD Initmap_3(void);
 void BuildrBarrier(int st,int end,int row);
 void BuildlBarrier(int st,int end,int line);//辅助造墙函数 
 void PlayerMove(int twd,struct COD* now);
-int GetPlayerCommand(void);
-void Judge(int twd,struct COD* now,struct COD* New);
-int BJudge(struct COD* Bnow,struct COD* BNew);
+int GetPlayerCommand(void);                //读取用户命令 
+void Judge(int twd,struct COD* now,struct COD* New);//判断人的移动 
+void BJudge(struct COD* Bnow,struct COD* BNew);//判断箱子移动 
 void BoxMove(int Btwd,struct COD* Bnow);
-void Put(void);
+void Put(void);//输出二维数组的当前值 
 
 int main(void){
-	struct COD* now;
+	struct COD* now;                         
 	struct COD loc=Initmap_1();
 	now = &loc;
 	
@@ -37,7 +37,7 @@ int main(void){
 		PlayerMove(GetPlayerCommand(),now);
 	}
 	Put();
-	printf("you win!,turn to level 2\n"); 
+	printf("you win!,turn to level 2！\n"); 
 	Sleep(1000);
 	
 	system("cls");
@@ -89,14 +89,18 @@ void BuildrBarrier(int st,int end,int row){
 	}
 }
 
-struct COD Initmap_1(void){                       //初始化第一关地图 
-	int i,j;
-	struct COD start;
+void Initmap(void){
+	int i,j;	
 	for(i=1;i<WIDE;i++){
 		for(j=1;j<LEN;j++){
 			MAP[i][j]=' ';
 		}
 	} 
+}
+
+struct COD Initmap_1(void){                       
+	struct COD start;
+	Initmap();
 	BuildrBarrier(2,4,0);
 	BuildlBarrier(1,3,2);
 	BuildrBarrier(0,1,3);
@@ -114,19 +118,13 @@ struct COD Initmap_1(void){                       //初始化第一关地图
 	end[2]=(struct COD){3,6};
 	end[3]=(struct COD){6,4}; 
 	start =(struct COD){4,4};
-	
 	return start;
 }
 
 struct COD Initmap_2(void){
-	end[3]=(struct COD){0,0};
-	int i,j;
-	struct COD start;
-	for(i=1;i<WIDE;i++){
-		for(j=1;j<LEN;j++){
-			MAP[i][j]=' ';
-		}
-	} 
+	end[3]=(struct COD){0,0};          //此关只有三个箱子 
+	struct COD start;              
+	Initmap();
 	BuildrBarrier(0,4,0);
 	BuildlBarrier(1,4,0);
 	BuildlBarrier(4,8,1);
@@ -144,18 +142,12 @@ struct COD Initmap_2(void){
 	end[1]=(struct COD){4,7};
 	end[2]=(struct COD){5,7};
 	start=(struct COD){1,1};
-	
 	return start;
 } 
 
 struct COD Initmap_3(void){
-	int i,j;
 	struct COD start;
-	for(i=1;i<WIDE;i++){
-		for(j=1;j<LEN;j++){
-			MAP[i][j]=' ';
-		}
-	} 
+	Initmap();
 	BuildrBarrier(1,7,0);
 	BuildrBarrier(0,5,2);
 	BuildlBarrier(3,5,0);
@@ -173,7 +165,6 @@ struct COD Initmap_3(void){
 	end[2]=(struct COD){5,2};
 	end[3]=(struct COD){5,3};
 	start =(struct COD){3,2};
-	
 	return start;
 }
 
@@ -192,19 +183,13 @@ int GetPlayerCommand(void){
 	return c;
 }
 
-void Judge(int twd,struct COD* now,struct COD* New){                      //判断人的移动  
+void Judge(int twd,struct COD* now,struct COD* New){                    
 	char temp;
-	if(MAP[New->x][New->y] == ' ' ){
-		temp=MAP[now->x][now->y];
-		MAP[now->x][now->y]=MAP[New->x][New->y];
-		MAP[New->x][New->y]=temp;
-		*now = *New;
-	}
-	if(MAP[New->x][New->y] == 'X'){
+	if(MAP[New->x][New->y] == ' ' || MAP[New->x][New->y] == 'X' ){
 		MAP[New->x][New->y]='o';
 		MAP[now->x][now->y]=' ';
-		*now = *New;	
-	} 
+		*now = *New;
+	}
 	if(cnt){
 		if(MAP[New->x][New->y] == 'B'){
 			struct COD *Bnow=New;
@@ -213,7 +198,7 @@ void Judge(int twd,struct COD* now,struct COD* New){                      //判断
 			cnt=1;
 		}
 	}
-	for(int i=0;i<4;i++){                            //避免X被吞掉 
+	for(int i=0;i<4;i++){                              //回复X的值 
 		if(MAP[end[i].x][end[i].y] == ' '){
 			MAP[end[i].x][end[i].y] = 'X';
 		}
@@ -232,15 +217,9 @@ void PlayerMove(int twd,struct COD* now){
 	}
 }
 
-int BJudge(struct COD* Bnow,struct COD* BNew){
+void BJudge(struct COD* Bnow,struct COD* BNew){
 	char temp;
-	if(MAP[BNew->x][BNew->y] == ' '){
-		temp=MAP[Bnow->x][Bnow->y];
-		MAP[Bnow->x][Bnow->y]=MAP[BNew->x][BNew->y];
-		MAP[BNew->x][BNew->y]=temp;
-		*Bnow = *BNew;
-	}
-	if(MAP[BNew->x][BNew->y] == 'X'){                              
+	if(MAP[BNew->x][BNew->y] == 'X' || MAP[BNew->x][BNew->y] == ' '){                              
 		MAP[BNew->x][BNew->y] = 'B';
 		MAP[Bnow->x][Bnow->y] = ' ';
 		*Bnow = *BNew;
@@ -248,10 +227,9 @@ int BJudge(struct COD* Bnow,struct COD* BNew){
 	if(MAP[BNew->x][BNew->y] == '#' || MAP[BNew->x][BNew->y] == 'B'){
 		cnt = 0;
 	}
-	return cnt;
 }
 
-void BoxMove(int Btwd,struct COD* Bnow){                        //箱子移动 
+void BoxMove(int Btwd,struct COD* Bnow){                        
 	struct COD m =*Bnow;
 	struct COD *BNew=&m;
 	switch(Btwd){
