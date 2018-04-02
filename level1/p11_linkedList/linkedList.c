140ed6894479ee0ba95a8ab46fd39b00d8f27f1c
fire-sale <gaofan0@126.com>
2018-04-02 15:57:33 +0800

    complete linkedlist

--- level1/p11_linkedList/linkedList.c
+++ level1/p11_linkedList/linkedList.c
@@ -1,28 +1,37 @@
 #include<stdio.h>
-#define N 100
-#define M 10
+#define N 10000
 struct node
 {
 	int v,next;
-};
-node a[N+1];
-int head;
-void rotate(){
-	int x=0,y=0;
-	for (int i=head;i;i=a[i].next)	
-		a[y].next=x,x=y,y=i;
-	a[y].next=x;
-	head=y;
+}d[N+1];
+int head,i,flag=0,n;
+void reverse(){
+	int tmp=0,v=0;
+	for(int i=head;i;i=d[i].next)
+	{
+		d[v].next = tmp;
+		tmp = v;
+		v = i;
+	}
+	d[v].next = tmp;
+	head = v;
 }
 int main(){
-	srand((int)time(0));
-	for (int i=1;i<=N;i++)a[i].v=rand()%M;
-	for (int i=1;i<N;i++)a[i].next=i+1;
-	head=1;
-	rotate();
-	for (int i=head;i;i=a[i].next)
-		if (a[i].v==5)printf("%d\n",i);
-	printf("-1");
+	scanf("%d", &n);
+	for(i=1;i<=n;i++)
+		d[i].v = i;
+	for(i=1;i<n;i++)
+		d[i].next=i+1;
+	head = 1;
+	reverse();
+	for (int i=head;i;i=d[i].next)
+		if (d[i].v == 5)
+		{
+			flag = 1;
+			printf("%d\n",i);
+		}
+	if(!flag) printf("-1\n");
 	return 0;
 }
+
 
