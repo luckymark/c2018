2632ff26a10454e889ac0d7ed527bf817782432d
smallhhy <37150104+smallhhy@users.noreply.github.com>
2018-04-22 20:22:14 +0800

    Update hanoi.c

--- level1/p08_hanoi/hanoi.c
+++ level1/p08_hanoi/hanoi.c
@@ -6,7 +6,7 @@ void move(int n,int x,int y,int z)
         printf("%c->%c\n",x,z);
     else
     {
-        move(n-1,x,y,z);
+        move(n-1,x,z,y);
         printf("%c->%c\n",x,z);
         move(n-1,y,x,z);
     }
@@ -15,8 +15,10 @@ void move(int n,int x,int y,int z)
 int main(void)
 {
     int n;
+    printf("Please input the number of dishes:\n");
     scanf("%d",&n);
     move(n,'a','b','c');
+    printf("Done!\n") ;
     getchar();
     getchar();
     return 0;
